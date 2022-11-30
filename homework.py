0563c10b79d09baa2d9363fd4842d07e1508bc54
Umanets Liliya <umanets.lilia@gmail.com>
2022-11-30 23:20:05 +0300

    pep8

--- homework.py
+++ homework.py
@@ -34,24 +34,28 @@ logging.basicConfig(
 )
 print(logging.info)
 
+
 def logger_error(message_error):
+    """Отправляет сообщение об ошибке в чат, возвращает логгер."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     bot.send_message(TELEGRAM_CHAT_ID, message_error)
     return logging.error(message_error)
 
+
 def check_tokens():
-    """Проверяет доступность переменных окружения"""
+    """Проверяет доступность переменных окружения."""
     tokens = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
-    filtered = (list(filter(None,tokens)))
+    filtered = (list(filter(None, tokens)))
     if len(filtered) != len(tokens):
         logging.critical('Отсутствует обязательная переменная окружения.'
-        'Программа принудительно остановлена.')
+            'Программа принудительно остановлена.')
         return False
     else:
         return True
 
+
 def send_message(bot, message):
-    """Отправляет сообщение в Telegram чат"""
+    """Отправляет сообщение в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except Exception as error:
@@ -61,13 +65,13 @@ def send_message(bot, message):
 
 
 def get_api_answer(current_timestamp):
-    """Делает запрос к эндпоинту API-сервиса"""
+    """Делает запрос к эндпоинту API-сервиса."""
     try:
         response = requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params={'from_date':current_timestamp}
-    )
+            ENDPOINT,
+            headers=HEADERS,
+            params={'from_date': current_timestamp}
+        )
     except Exception as error:
         logger_error(f'Сбой при запросе к эндпоинту - {error}')
     if response.status_code != 200:
@@ -77,7 +81,7 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
-    """Проверяет ответ API на соответствие документации"""
+    """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
         raise TypeError('получен неверный тип данных')
     homeworks = response.get('homeworks')
@@ -87,7 +91,7 @@ def check_response(response):
 
 
 def parse_status(homework):
-    """Извлекает из информации о конкретной домашней работе статус этой работы"""
+    """Извлекает из информации о домашней работе статус этой работы."""
     if 'homework_name' not in homework:
         raise KeyError('в ответе API нет ключа homework_name')
     if 'status' not in homework:
@@ -104,7 +108,7 @@ def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-    if check_tokens() == False:
+    if check_tokens() is False:
         sys.exit()
     while True:
         try:
@@ -115,7 +119,7 @@ def main():
                 message = parse_status(homeworks[0])
                 send_message(bot, message)
             else:
-                logging.debug(f'отсутствие в ответе новых статусов')
+                logging.debug (f'отсутствие в ответе новых статусов')
         except Exception as error:
             last_error = ''
             message_error = (f'Сбой в работе программы: {error}')
@@ -124,6 +128,7 @@ def main():
                 send_message(bot, message_error)
                 last_error = error
         time.sleep(RETRY_PERIOD)
+
 
 if __name__ == '__main__':
     main()
